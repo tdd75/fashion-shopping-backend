296bc5f8167ecfe9ab88373a98e8fffa576148bf
tdd75 <tranducduy7520@gmail.com>
2023-02-28 18:18:30 +0000

    feat: update order admin

--- product_variants/admin.py
+++ product_variants/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from .models import ProductVariant
 
 
-admin.site.register(ProductVariant)
+@admin.register(ProductVariant)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'color', 'size', 'stocks','price')
+    ordering = ('-created_at',)
+    search_fields = ('color', 'size')
